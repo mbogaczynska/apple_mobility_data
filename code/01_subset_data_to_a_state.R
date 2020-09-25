10436f294266305629987368fef0856daf57a87e
Marta Bogaczynska <mgbogaczynska@usfca.edu>
2020-09-24 19:37:52 -0700

    Turned the script into a for loop

--- code/01_subset_data_to_a_state.R
+++ code/01_subset_data_to_a_state.R
@@ -10,7 +10,15 @@
 # load functions
 source("code/functions/subset_mobility_data_to_state.R")
 
+# use the function in a for loop
+for (state_to_subset in c("Ohio", "Arizona", "Nevada", "Alaska", "Minnesota")){
+  subset_mobility_data_to_state(
+    input_file_name = "data/raw_data/applemobilitytrends-2020-09-17.csv",
+    state_to_subset)
+}
+
 # test out the use of the function
 subset_mobility_data_to_state(
   input_file_name = "data/raw_data/applemobilitytrends-2020-09-11.csv",
   state_to_subset = "California")
+
