6e83ed7f634c65c181397dbc0715991af5f21743
Marta Bogaczynska <mgbogaczynska@usfca.edu>
2020-10-17 12:16:34 -0700

    Add if statement to check for parameters

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -10,6 +10,14 @@
 # October 17, 2020
 # mgbogaczynska@usfca.edu
 
+if [ $# -eq 0 ]
+then
+  echo "To run this script, supply two arguments:"
+  echo "The first is the name of the state to be analyzed."
+  echo "The second is the path to the mobility data csv file."
+  exit 1
+fi
+
 Rscript -e "rmarkdown::render('Analysis.Rmd',\
             params = list(state = '$1',\
             data = '$2'))"
