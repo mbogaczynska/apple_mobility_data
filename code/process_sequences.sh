8bdab22e6821543c2388b5f6a62f1cdca4890615
Marta Bogaczynska <mgbogaczynska@usfca.edu>
2020-10-31 03:00:34 +0000

    Start working again on bash script

---
+++ code/process_sequences.sh
@@ -0,0 +1,7 @@
+# Marta Bogaczynska
+# mgbogaczynska@usfca.edu
+# October 30, 2020
+
+# This is a bash script that will calculate the total number of sequences in the 
+# 2020-10-15_ncbi_sars_cov_2_sequences.fasta.gz file and then tally up the number of SARS-CoV-2 
+# sequences from each country and sort the output from largest to smallest.
