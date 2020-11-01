4f601a4a5dd1551ea14360712c257b61c7797f8f
Marta Bogaczynska <mgbogaczynska@usfca.edu>
2020-11-01 21:11:30 +0000

    Add process sequences bash script

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -1,7 +1,17 @@
-# Marta Bogaczynska
-# mgbogaczynska@usfca.edu
-# October 30, 2020
+#!/bin/bash
 
-# This is a bash script that will calculate the total number of sequences in the 
-# 2020-10-15_ncbi_sars_cov_2_sequences.fasta.gz file and then tally up the number of SARS-CoV-2 
-# sequences from each country and sort the output from largest to smallest.
+# Marta Bogaczynska mgbogaczynska@usfca.edu October 30, 2020
+
+# This is a bash script that will calculate the total number 
+# of sequences in the 
+# 2020-10-15_ncbi_sars_cov_2_sequences.fasta.gz file and then 
+# tally up the number of SARS-CoV-2 sequences from each 
+# country and sort the output from largest to smallest.
+
+fastafile=$1
+
+# The number of sequences should be 28843 
+echo "This is the number of sequences in the fasta file" 
+zcat $fastafile | grep -c "^>"
+
+bioawk -c fastx '{print $comment}' $fastafile | cut -d "|" -f 3 | sort | uniq -c | sort -rn 
